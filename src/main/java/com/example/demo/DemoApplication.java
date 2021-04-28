a064b46757b2ac80735f34da5e7e6f183893fdb4
anhtam.nguyen <anhtamit2019@gmail.com>
2021-04-28 11:06:39 +0700

    push

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import java.util.TimeZone;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
